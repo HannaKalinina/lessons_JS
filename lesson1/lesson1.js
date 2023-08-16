a40ad8575996452c9b2c4aa009dc70ad3f92ee53
HannaKalinina <excelsior.22xii@gmail.com>
2023-08-16 12:17:40 +0200

    lesson 1

---
+++ lesson1/lesson1.js
@@ -0,0 +1,52 @@
+let a1=`hello`;
+console.log(a1);
+let a2=`owu`;
+console.log(a2);
+let a3=`com`;
+console.log(a3);
+let a4=`ua`;
+console.log(a4);
+let a5=1;
+console.log(a5);
+let a6=10;
+console.log(a6);
+let a7=-999;
+console.log(a7);
+let a8=123;
+console.log(a8);
+let a9=3.14;
+console.log(a9);
+let a10=2.7;
+console.log(a10);
+let a11=16;
+console.log(a11);
+let a12=true;
+console.log(a12);
+let a13=false;
+console.log(a13);
+
+let firstName;
+firstName=`Калинина`;
+let middleName;
+middleName=`Анна`;
+let lastName;
+lastName=`Владимировна`;
+let person=`${firstName} ${middleName} ${lastName}`;
+console.log(person);
+
+let a=100;
+console.log(typeof a);
+let b='100';
+console.log(typeof b);
+let c=true;
+console.log(typeof c);
+
+let nameUser=prompt('enter your first name');
+let surname=prompt('enter your last name');
+let ageUser=prompt('enter your age');
+let user=nameUser+' '+surname+' '+ageUser;
+console.log(user);
+
+
+
+
